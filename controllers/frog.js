d4717af93410678d8d0fa11ecffaaa56667f5c27
Ramya Devulapally <S547918@nwmissouri.edu>
2022-11-10 22:13:08 -0600

    updated frog file

--- controllers/frog.js
+++ controllers/frog.js
@@ -3,9 +3,16 @@ var Frog = require('../models/frog');
 exports.frog_list = function (req, res) {
     res.send('NOT IMPLEMENTED: Frog list');
 };
-// for a specific Frog.
-exports.frog_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Frog detail: ' + req.params.id);
+// for a specific Costume. 
+exports.frog_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Frog.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    }
 };
 // Handle Frog create on POST.
 exports.frog_create_post = function (req, res) {
@@ -62,4 +69,24 @@ exports.frog_create_post = async function (req, res) {
         res.status(500);
         res.send(`{"error": ${err}}`);
     }
+}; 
+
+// Handle Costume update form on PUT. 
+exports.frog_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Frog.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.age) toUpdate.age = req.body.age; 
+        if(req.body.color) toUpdate.color = req.body.color; 
+        if(req.body.lifeSpan) toUpdate.lifeSpan = req.body.lifeSpan; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
 }; 
