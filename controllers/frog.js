fdfd633440b7fee70aef11b13820cefab0f89f26
Ramya Devulapally <S547918@nwmissouri.edu>
2022-11-16 19:48:17 -0600

    updated frog

--- controllers/frog.js
+++ controllers/frog.js
@@ -3,7 +3,7 @@ var Frog = require('../models/frog');
 exports.frog_list = function (req, res) {
     res.send('NOT IMPLEMENTED: Frog list');
 };
-// for a specific Costume. 
+// for a specific Frog. 
 exports.frog_detail = async function(req, res) { 
     console.log("detail"  + req.params.id) 
     try { 
@@ -50,14 +50,14 @@ exports.frog_view_all_Page = async function (req, res) {
     }
 };
 
-// Handle Costume create on POST. 
+// Handle Frog create on POST. 
 exports.frog_create_post = async function (req, res) {
     console.log(req.body)
     let document = new Frog();
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
-    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    // {"frog_type":"goat", "cost":12, "size":"large"} 
     document.age = req.body.age;
     document.color = req.body.color;
     document.lifeSpan = req.body.lifeSpan;
@@ -71,7 +71,7 @@ exports.frog_create_post = async function (req, res) {
     }
 }; 
 
-// Handle Costume update form on PUT. 
+// Handle Frog update form on PUT. 
 exports.frog_update_put = async function(req, res) { 
     console.log(`update on id ${req.params.id} with body 
 ${JSON.stringify(req.body)}`) 
@@ -90,3 +90,74 @@ ${JSON.stringify(req.body)}`)
 failed`); 
     } 
 }; 
+
+// Handle Frog delete on DELETE. 
+exports.frog_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Frog.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+}; 
+
+
+ // Handle a show one view with id specified by query 
+ exports.frog_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await Frog.findById( req.query.id) 
+        res.render('frogdetail',  
+{ title: 'Frog Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+ // Handle building the view for creating a frog. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.frog_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('frogcreate', { title: 'Frog Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for updating a frog.
+// query provides the id
+exports.frog_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Frog.findById(req.query.id)
+    res.render('frogupdate', { title: 'Frog Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle a delete one view with id from query 
+    exports.frog_delete_Page = async function(req, res) { 
+        console.log("Delete view for id "  + req.query.id) 
+        try{ 
+            result = await Frog.findById(req.query.id) 
+            res.render('frogdelete', { title: 'Frog Delete', toShow: 
+    result }); 
+        } 
+        catch(err){ 
+            res.status(500) 
+            res.send(`{'error': '${err}'}`); 
+        } 
+    }; 
+
--- routes/frog.js
+++ routes/frog.js
@@ -9,6 +9,19 @@
 var express = require('express');
 const frog_controlers= require('../controllers/frog');
 var router = express.Router();
-/* GET costumes */
+/* GET frogs */
 router.get('/', frog_controlers.frog_view_all_Page );
 module.exports = router;
+
+/* GET detail frog page */ 
+router.get('/detail', frog_controlers.frog_view_one_Page); 
+
+/* GET create frog page */ 
+router.get('/create', frog_controlers.frog_create_Page); 
+ 
+/* GET create update page */
+router.get('/update', frog_controlers.frog_update_Page);
+
+/* GET delete frog page */ 
+router.get('/delete', frog_controlers.frog_delete_Page); 
+
